e7983df9f1fece307d171b55b48f90d0274d290f
Rodolfo Prieto <rhantoni.cuba@gmail.com>
2026-02-13 19:27:13 +0100

    Fix: ignore Next build output and node_modules in src/Front

--- src/Front/.next/server/server-reference-manifest.js
+++
@@ -1,1 +0,0 @@
-self.__RSC_SERVER_MANIFEST="{\n  \"node\": {},\n  \"edge\": {},\n  \"encryptionKey\": \"nyh9jfPGKqRvCaOQOZG/hIxF9LCkf3PIrnlLioNh1Lo=\"\n}"
--- src/Front/.next/static/chunks/app_page_tsx_2fc2083d._.js
+++
@@ -1,8 +0,0 @@
-(globalThis.TURBOPACK_CHUNK_LISTS || (globalThis.TURBOPACK_CHUNK_LISTS = [])).push({
-    script: typeof document === "object" ? document.currentScript : undefined,
-    chunks: [
-  "static/chunks/node_modules_13e5b2fe._.js",
-  "static/chunks/_55d8411a._.js"
-],
-    source: "dynamic"
-});
